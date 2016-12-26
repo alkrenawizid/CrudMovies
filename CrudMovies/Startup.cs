a3408c9caf3edd91042163899e55fd1e90e33c62
Alkrenawi zid <z.i.d@msn.com>
2016-12-26 16:21:40 +0200

    Add project files.

---
+++ CrudMovies/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(CrudMovies.Startup))]
+namespace CrudMovies
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
